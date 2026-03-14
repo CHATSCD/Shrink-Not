25d497691e5cece6d29157822cd727e3c27edeaa
CHATSCD <shaundubuisson24@gmail.com>
2026-03-13 21:10:24 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
 const SUPABASE_URL = "https://ylfsqjbqhewpfiooytlg.supabase.co";
-const SUPABASE_KEY = "YOUR_SUPABASE_ANON_KEY";
+
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlsZnNxamJxaGV3cGZpb295dGxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NzQwODQsImV4cCI6MjA4ODM1MDA4NH0.Ssl5J6ktZIVtLd2cq8vH7PWdR4-yZwI5tt4PtXkVqWk";
 
 const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
